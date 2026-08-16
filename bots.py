3a0627e2e03bb791ebbea8358c772c18466b0668
dimaghost25 <shishenkovd@gmail.com>
2026-08-16 21:18:29 +0900

    Update bots.py

--- bots.py
+++ bots.py
@@ -136,7 +136,11 @@ def get_family_report(guild_id: int):
 async def fetch_json(url: str, headers=None):
     default_headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
-        "Accept": "application/json, text/plain, */*"
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "cross-site"
     }
     if headers:
         default_headers.update(headers)
